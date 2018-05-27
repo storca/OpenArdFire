3df73ba718b8dde5d3fe181154b39375d9ee6eb9
storca <root@localhost.localdomain>
2018-05-27 11:55:56 +0200

    Draft

--- lib/FiringModule/FiringModule.cpp
+++ lib/FiringModule/FiringModule.cpp
@@ -1,1 +1,16 @@
 #include "FiringModule.h"
+
+FiringModule::FiringModule(int &cues, int &testPins, Logger &log)
+{
+  this->_cuePins = &cues;
+  this->_logger = &log;
+  this->_testPins = &testPins;
+
+  this->_logger->info("This firing system has " + String(sizeof(*this->_cuePins)) + " cues");
+  this->_logger->info("Test pins are ");
+}
+
+FiringModule::~FiringModule()
+{
+
+}
--- lib/FiringModule/FiringModule.h
+++ lib/FiringModule/FiringModule.h
@@ -1,15 +1,21 @@
 #ifndef FIRINGMODULE_H
 #define FIRINGMODULE_H
 
+#define FM_NB_SECURITIES 3
+
 #include <Arduino.h>
+#include <Logging.h>
 
 class FiringModule
 {
 public:
-  FiringModule();
+  FiringModule(int &cues, int &testPins, Logger &log);
   ~FiringModule();
 private:
-  
+  int *_cuePins;
+  int *_testPins;
+  bool _securities[FM_NB_SECURITIES];
+  Logger *_logger;
 };
 
 #endif
