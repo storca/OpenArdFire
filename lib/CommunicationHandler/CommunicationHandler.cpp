b451943b7560229f07d8a1811a66e8b97aea6c72
storca <geek392662@gmail.com>
2018-06-07 21:31:40 +0200

    Bug Fix

--- lib/CommunicationHandler/CommunicationHandler.cpp
+++ lib/CommunicationHandler/CommunicationHandler.cpp
@@ -18,6 +18,7 @@ void CommunicationHandler::handler()
     if(character == '\n')
     {
       //processCommand(); to do
+      this->_localSerialMessage = "";
     }
     else
     {
@@ -32,6 +33,7 @@ void CommunicationHandler::handler()
     if(character == '\n')
     {
       //processCommand(); to do
+      this->_rfSerialMessage = "";
     }
     else
     {
